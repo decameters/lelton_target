4f387622b1e7514f083ceb125f7bbfd5ad1e489c
liz elton <eeeqqqeee@gmail.com>
2018-04-04 10:22:43 -0500

    build out jeff bridges db

--- myretail.js
+++ myretail.js
@@ -4,9 +4,54 @@ db.createCollection('items');
 
 db.items.insert({
     id : 13860428 ,
-    title : "The Big Lebowski" ,
+    title : "TRON" ,
     current_price : {
-        value : 13.49,
-        currency_code : "USD",
+        value : 13.49 ,
+        currency_code : "USD" ,
     }
-    });
+});
+
+db.items.insert({
+    id : 15117729 ,
+    title : "Hearts of the West" ,
+    current_price : {
+        value : 10.49 ,
+        currency_code : "USD" ,
+    }
+});
+
+db.items.insert({
+    id : 16483589 ,
+    title : "The Last Unicorn" ,
+    current_price : {
+        value : 13.49 ,
+        currency_code : "USD" ,
+    }
+});
+
+db.items.insert({
+    id : 16696652 ,
+    title : "The Fabulous Baker Boys" ,
+    current_price : {
+        value : 11.49 ,
+        currency_code : "USD" ,
+    }
+});
+
+db.items.insert({
+    id : 16752456 ,
+    title : "White Squall" ,
+    current_price : {
+        value : 10.49 ,
+        currency_code : "USD" ,
+    }
+});
+
+db.items.insert({
+    id : 15643793 ,
+    title : "Seabiscuit" ,
+    current_price : {
+        value : 13.49 ,
+        currency_code : "USD" ,
+    }
+});
